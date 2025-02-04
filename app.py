022c5d7f0bfdd106b803e942929b5372f9254bb3
San <sandy_chromero@hotmail.com>
2025-02-04 17:24:17 -0600

    archivo

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ import pandas as pd
 import plotly_express as px
 vehicle_data = pd.read_csv("vehicles_us.csv")
 st.header('Datos de anuncios de vehículos en venta')
+
 build_histogram = st.checkbox('Construir histograma')
 if build_histogram:
     st.write('Creación de un histograma para la columna odómetro')
     fig = px.histogram(vehicle_data, x="odometer")
     st.plotly_chart(fig, use_container_width=True)
+
 graf_button = st.button('Construir gráfico de dispersión')
 if graf_button:
     st.write = ('Creación de un gráfico de dispersión ')
